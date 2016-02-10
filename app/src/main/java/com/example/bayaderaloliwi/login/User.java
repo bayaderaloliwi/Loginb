7c1ea043bb6e58d3112b66cfdae69227c992e890
bayaderaloliwi <bayader-aloliwi@live.com>
2016-02-10 12:46:31 +0300

    Initial commit

---
+++ app/src/main/java/com/example/bayaderaloliwi/login/User.java
@@ -0,0 +1,23 @@
+package com.example.bayaderaloliwi.login;
+
+/**
+ * Created by bayaderaloliwi on 2/9/2016.
+
+/**
+ * Created by Noura on 2/5/2016.
+ */
+public class User {
+    String name, password, email;
+    int id;
+
+    public User (int id, String name, String password, String email){
+
+        this.id = id;
+        this.name= name;
+        this.password= password;
+        this.email= email;
+
+    }
+
+}
+
---
+++ app/src/main/java/com/example/bayaderaloliwi/login/backgroundtask.java
@@ -0,0 +1,7 @@
+package com.example.bayaderaloliwi.login;
+
+/**
+ * Created by bayaderaloliwi on 2/9/2016.
+ */
+public class backgroundtask {
+}
